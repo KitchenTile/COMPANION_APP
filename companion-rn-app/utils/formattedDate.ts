d114db5cee7dd08d8e095596d3a145c0ed1f22a5
Blue <azuldebenedetti@gmail.com>
2025-12-07 00:04:27 +0000

    Deleted unused files

--- companion-rn-app/utils/formattedDate.ts
+++
@@ -1,9 +0,0 @@
-export const formattedDate = (date: string) => {
-    const d = date.substring(8, 10)
-    const m = date.substring(5, 7)
-    const y = date.substring(0, 4)
-
-    const formattedDate = `${d + "/" + m + "/" + y}`
-
-    return formattedDate;
-}
