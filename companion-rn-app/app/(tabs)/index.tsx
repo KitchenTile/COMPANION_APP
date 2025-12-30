da6e7900679d5cd19ab3b23f6e552254e8ac80c8
Blue <azuldebenedetti@gmail.com>
2025-12-30 15:51:30 +0000

    Update index.tsx

--- companion-rn-app/app/(tabs)/index.tsx
+++ companion-rn-app/app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
 import { Image } from "expo-image";
-import { Platform, StyleSheet } from "react-native";
+import { Platform, StyleSheet, TouchableOpacity, View } from "react-native";
+import * as Linking from "expo-linking";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
-import { getPythonBackend } from "@/api/fetchAPI";
+import * as WebBrowser from "expo-web-browser";
+import { getGmailLogin } from "@/api/fetchAPI";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function HomeScreen() {
+  const [ready, setReady] = useState(false);
+
   useEffect(() => {
     // const fetchStuff = async () => {
     //   try {
@@ -31,6 +37,30 @@ export default function HomeScreen() {
     // };
     // fetchStuff();
   });
+
+  const openGoogleLink = async () => {
+    // Linking.openURL("http://localhost:8000/gmailLogin");
+    await WebBrowser.openAuthSessionAsync(
+      "http://localhost:8000/gmailLogin",
+      "aicompanion://auth"
+    );
+    setReady(true);
+  };
+
+  useEffect(() => {
+    const sub = Linking.addEventListener("url", ({ url }) => {
+      const { queryParams } = Linking.parse(url);
+      console.log("here");
+
+      if (queryParams?.session) {
+        console.log("Session:", queryParams.session);
+        // exchange with backend here
+      }
+    });
+
+    return () => sub.remove();
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -45,40 +75,15 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+
+      <ThemedView>
+        <TouchableOpacity style={styles.gmailButton} onPress={openGoogleLink}>
+          <ThemedText
+            style={{ color: "white", textAlign: "center", fontSize: 20 }}
+          >
+            Link Gmail
+          </ThemedText>
+        </TouchableOpacity>
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -100,5 +105,13 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: "absolute",
+  },
+  gmailButton: {
+    padding: 10,
+    backgroundColor: "#DE5246",
+    borderRadius: 20,
+    borderColor: "rgba(193, 49, 36, 1)",
+    borderStyle: "solid",
+    borderWidth: 1,
   },
 });
