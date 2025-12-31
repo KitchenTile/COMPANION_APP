4a33f1efaa0fb97fd98eccc13d47e2038d53cdc7
Blue <azuldebenedetti@gmail.com>
2025-12-31 12:08:06 +0000

    Create store.ts

---
+++ companion-rn-app/store/store.ts
@@ -0,0 +1,113 @@
+import { supabase } from "@/supabase/supabase";
+import { AuthStore } from "@/utils/types";
+import { create } from "zustand";
+
+export const useAuthStore = create<AuthStore>((set, get) => ({
+  isLoggedIn: false,
+  isLoading: false,
+  user: null,
+  error: null,
+  session: null,
+
+  login: async (email: string, password: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        set({ error: error });
+        return;
+      }
+
+      console.log("user data");
+      console.log(data);
+
+      set({ user: data.user, isLoading: false, isLoggedIn: true });
+    } catch (error) {
+      set({ isLoading: false });
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        alert("Login failed");
+      }
+    }
+  },
+
+  signUp: async (name: string, email: string, password: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+          data: {
+            full_name: name,
+          },
+        },
+      });
+
+      if (error) {
+        set({ error: error });
+        return;
+      }
+
+      console.log("user data");
+      console.log(data);
+
+      set({ user: data.user, isLoading: false, isLoggedIn: !!data.session });
+    } catch (error) {
+      set({ isLoading: false });
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        alert("Signup failed");
+      }
+    }
+  },
+
+  logout: async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+
+      if (error) {
+        set({ error: error });
+      }
+
+      set({ user: null, isLoggedIn: false });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  initialize: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      set({
+        session: session,
+        user: session ? session.user : null,
+        isLoading: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      set({
+        session,
+        user: session ? session.user : null,
+        isLoading: false,
+      });
+    });
+
+    return () => subscription.unsubscribe();
+  },
+}));
