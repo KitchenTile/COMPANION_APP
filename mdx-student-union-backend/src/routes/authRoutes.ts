ae21c24a4d9e466a5854ef427bd52c5c64117fe4
Blue <118444162+KitchenTile@users.noreply.github.com>
2026-01-21 14:26:06 -0800

    Delete mdx-student-union-backend/src directory

--- mdx-student-union-backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { login } from "../controllers/authControllers";
-
-const authRouter = express.Router();
-
-authRouter.post("/login", login);
-
-export default authRouter;
--- mdx-student-union-backend/src/utils/jwt.ts
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateJWT = (payload: object) => {
-  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "12h" });
-};
