080faf2e4da870ac850b7134cdf5b5fb169a06a2
Blue <azuldebenedetti@gmail.com>
2026-02-01 23:09:56 +0000

    Update api.ts

--- companion-rn-app/utils/api.ts
+++ companion-rn-app/utils/api.ts
@@ -3,8 +3,8 @@ import { Platform } from "react-native";
 export const baseURL =
   Platform.OS === "android"
     ? "http://10.0.2.2:4000/api"
-    : // : "http://192.168.1.28:8000";
-      `https://isaac-preadditional-tirelessly.ngrok-free.dev`;
+    : "http://http://34.227.7.88:8000";
+// `https://isaac-preadditional-tirelessly.ngrok-free.dev`;
 
 export const WS_URL = baseURL
   .replace("https://", "wss://")
