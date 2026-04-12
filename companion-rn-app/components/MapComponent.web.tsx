66bbc28caa6eda0415e2d98b75ecb54a451d4c8a
Blue <azuldebenedetti@gmail.com>
2026-04-12 20:27:52 +0100

    Update MapComponent.web.tsx

--- companion-rn-app/components/MapComponent.web.tsx
+++ companion-rn-app/components/MapComponent.web.tsx
@@ -5,8 +5,6 @@ import { TreeNode } from "@/utils/types";
 import {
   Background,
   Controls,
-  Handle,
-  Position,
   ReactFlow,
   useEdgesState,
   useNodesState,
@@ -15,74 +13,9 @@ import "@xyflow/react/dist/style.css";
 import * as d3 from "d3-hierarchy";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect, useMemo, useState } from "react";
+import CustomNode from "./CustomNodes";
 import LoadingComponent from "./LoadingComponent.web";
 import NodeModal from "./NodeModal";
-
-const CustomNode = ({ data, isConnectable }: any) => {
-  const isPrevention = data.type === "prevention";
-  const isRisk = data.type === "risk";
-
-  return (
-    <div
-      style={{
-        ...data.style,
-        padding: "10px",
-        textAlign: "center",
-        minWidth: "100px",
-        borderRadius: 20,
-      }}
-    >
-      <Handle
-        type="target"
-        position={isPrevention ? Position.Bottom : Position.Top}
-        id={isPrevention ? "target-bottom" : "target-top"}
-        isConnectable={isConnectable}
-        style={{ background: isPrevention ? "#0070d3ff" : "#555" }}
-      />
-
-      <div
-        style={{
-          fontWeight: "bold",
-          fontSize: "12px",
-          marginBottom: 4,
-          maxWidth: 100,
-        }}
-      >
-        {data.label}
-      </div>
-
-      {!isPrevention && (
-        <div
-          style={{
-            fontSize: "8px",
-          }}
-        >
-          {data.probability < 1 ? data.probability : data.probability / 10}
-        </div>
-      )}
-
-      {!isPrevention && (
-        <Handle
-          type="source"
-          position={Position.Top}
-          id="source-top"
-          style={{ right: "30%", background: "#888" }}
-          isConnectable={isConnectable}
-        />
-      )}
-
-      {!isPrevention && (
-        <Handle
-          type="source"
-          position={Position.Bottom}
-          id="source-bottom"
-          style={{ left: "50%", background: "#555" }}
-          isConnectable={isConnectable}
-        />
-      )}
-    </div>
-  );
-};
 
 const getEdgeColor = (probability: number | undefined) => {
   if (!probability) return;
@@ -142,6 +75,7 @@ export default function App() {
     if (!graph) return;
     setTravelData(graph);
     setLoading(false);
+    console.log(graph);
   }, [graph]);
 
   const getTravelData = async () => {
